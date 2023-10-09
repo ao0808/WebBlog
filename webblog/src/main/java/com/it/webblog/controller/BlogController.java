013652fd1874c71ecc5799a09e935382e15f2d61
tokenpah@student.21-school.ru <tokenpah@student.21-school.ru>
2023-10-09 16:35:21 +0300

    +remove

--- webblog/src/main/java/com/it/webblog/controller/BlogController.java
+++ webblog/src/main/java/com/it/webblog/controller/BlogController.java
@@ -48,4 +48,35 @@ public class BlogController {
         model.addAttribute("post", list);
         return "blog-details";
     }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> list = new ArrayList<>();
+        post.ifPresent(list::add);
+        model.addAttribute("post", list);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id,
+                                 @RequestParam String title, @RequestParam String anons,
+                                 @RequestParam String full_text, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDel(@PathVariable(value = "id") long id, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
 }
